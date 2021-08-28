49fc8879510c1ee5c3817431508fdb957ff74b1f
project-haley <60005425+project-haley@users.noreply.github.com>
2021-08-27 21:36:03 -0500

    added base functionality

--- js/site.js
+++ js/site.js
@@ -1,41 +1,21 @@
-function getValues() {
-    let startValue = document.getElementById("startValue").value;
-    let endValue = document.getElementById("endValue").value;
-
-    startValue = parseInt(startValue);
-    endValue = parseInt(endValue);
-
-    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
-        let numbers = generateNumbers(startValue, endValue);
-        return numbers;
-    }
+function getString() {
+    var targetString = document.getElementById("targetString").value;
+    return targetString;
 }
 
-function generateNumbers(start, end) {
-    let numbers = [];
+function reverseString() {
+    var targetString = getString();
+    var targetStringReverse = ""
 
-    for (let index = start; index <= end; index++) {
-        numbers.push(index);
-    }
+    for(let i = targetString.length - 1; i >= 0; i--) {
+        targetStringReverse += targetString[i];
+    };
 
-    return numbers;
+    return targetStringReverse;
 }
 
-function generateTable() {
-    let numbers = getValues();
-    let tableResult = document.getElementById("results")
-    let tableString = ""
-    let className = "even"
-
-    for (index = numbers[0]; index <= numbers[numbers.length - 1]; index++) {
-        if (index % 2 == 0) {
-            className = "even";
-        } else {
-            className = "odd";
-        }
-        tableString += `<tr><td class="${className}">${index}</td></tr>`;
-
-    }
-
-    tableResult.innerHTML = tableString;
+function printReverseString() {
+    var printHeader = document.getElementById("results");
+    
+    printHeader.innerHTML = reverseString();
 }
